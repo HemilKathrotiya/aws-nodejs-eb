75d9d8ad9beaee1e943dd21fb26514757e1bc70b
HemilKathrotiya <103138559+HemilKathrotiya@users.noreply.github.com>
2024-05-09 12:50:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'S3 CloudHub YT channel & Elastic Beanstalk'
+    title: 'Hemil Kathrotiya Devops Project is running successfully'
     })
     res.status(200).end();
 })
